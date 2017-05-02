aa39269a458ee061a5e8c4afdfae7c5618301ae4
[01;31m[K"name"[m[K: "Nick Himmel <[01;31m[K"email"[m[K: null>
2017-05-02 13:47:31 +0200

    Add Main class

---
+++ app/components/Main.jsx
@@ -0,0 +1,15 @@
+var React = require('react');
+
+var Main = React.createClass({
+  render: function () {
+    return (
+      <div>
+        <h1>Nick Himmel</h1>
+        <h2>web development</h2>
+        <h2>photography</h2>
+      </div>
+    )
+  }
+})
+
+module.exports = Main;

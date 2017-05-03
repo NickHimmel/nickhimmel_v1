28d695425cfad17c555377494b5d669b4d08547f
[01;31m[K"name"[m[K: "Nick Himmel <[01;31m[K"email"[m[K: null>
2017-05-03 18:18:39 +0200

    Add nav

---
+++ app/components/Nav.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Nav extends React.Component {
+  render() {
+    return (
+      <div>
+        <h2>Nav Bar</h2>
+      </div>
+    )
+  }
+}
+
+export default Nav;

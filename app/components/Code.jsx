e411214e32e39b9ab0ce6ef12fed871fc4848a4e
[01;31m[K"name"[m[K: "Nick Himmel <[01;31m[K"email"[m[K: null>
2017-05-04 15:46:14 +0200

    Update

--- app/components/Code.jsx
+++ app/components/Code.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import Nav from 'app/components/Nav.jsx'
 
 class Code extends React.Component {
   render() {
     return (
       <div>
+        <Nav />
         <h2>Code</h2>
       </div>
     )

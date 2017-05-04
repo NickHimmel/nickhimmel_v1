ce864d7c7e69b32d114f341e3a0cf4658b6e2e67
[01;31m[K"name"[m[K: "Nick Himmel <[01;31m[K"email"[m[K: null>
2017-05-04 16:40:08 +0200

    Add home link on nav bar

--- app/components/Nav.jsx
+++ app/components/Nav.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import {Link} from 'react-router';
 
 class Nav extends React.Component {
   render() {
     return (
       <div>
-        <h2>Nick Himmel</h2>
+        <h2><Link to="/" activeClassName="active">Nick Himmel</Link></h2>
         <ul>
           <li>Web Projects</li>
           <li>Photo Projects</li>
--- public/bundle.js
+++ public/bundle.js
@@ -25048,6 +25048,8 @@
 
 	var _react2 = _interopRequireDefault(_react);
 
+	var _reactRouter = __webpack_require__(159);
+
 	function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 	function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
@@ -25074,7 +25076,11 @@
 	        _react2.default.createElement(
 	          'h2',
 	          null,
-	          'Nick Himmel'
+	          _react2.default.createElement(
+	            _reactRouter.Link,
+	            { to: '/', activeClassName: 'active' },
+	            'Nick Himmel'
+	          )
 	        ),
 	        _react2.default.createElement(
 	          'ul',

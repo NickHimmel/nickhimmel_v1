5e1b6cf8d7eff9c17d09f53bf0522700c56cc852
[01;31m[K"name"[m[K: "Nick Himmel <[01;31m[K"email"[m[K: null>
2017-05-08 19:05:31 +0200

    Add footer

--- app/components/Portfolio.jsx
+++ app/components/Portfolio.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Nav from 'app/components/Nav.jsx'
+import Footer from 'app/components/Footer.jsx'
 
 class Portfolio extends React.Component {
   render() {
@@ -9,6 +10,7 @@ class Portfolio extends React.Component {
         <div className="portfolio">
           {this.props.children}
         </div>
+        <Footer />
       </div>
     )
   }

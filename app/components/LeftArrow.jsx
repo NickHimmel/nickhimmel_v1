aaea37cb4a676abd299437a668ec826fb590d4d5
[01;31m[K"name"[m[K: "Nick Himmel <[01;31m[K"email"[m[K: null>
2017-06-17 16:05:19 -0400

    Add react to arrow components

---
+++ app/components/LeftArrow.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class LeftArrow extends React.Component {
+  render() {
+    return (
+
+    )
+  }
+}
+
+export default LeftArrow;
---
+++ app/components/RightArrow.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class RightArrow extends React.Component {
+  render() {
+    return (
+
+    )
+  }
+}
+
+export default RightArrow;

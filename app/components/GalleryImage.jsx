949aedf5a106a9187ca78a29e51e66eda556a9cc
[01;31m[K"name"[m[K: "Nick Himmel <[01;31m[K"email"[m[K: null>
2017-06-27 15:31:23 -0400

    Turn gallery images into stateless compoenents

--- app/components/GalleryImage.jsx
+++ app/components/GalleryImage.jsx
@@ -1,12 +1,9 @@
 import React from 'react';
 
-class GalleryImage extends React.Component {
-  render() {
-    let {src, alt} = this.props;
-    return (
-      <li> <img className="images" src={src} alt={alt}/> </li>
-    )
-  }
+const GalleryImage = ({src, alt}) => {
+  return (
+    <li> <img className="images" src={src} alt={alt}/> </li>
+  )
 }
 
 export default GalleryImage;

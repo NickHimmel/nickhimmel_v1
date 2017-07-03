fe81c9b0e69def67ddf89e69184a1184cbc37287
[01;31m[K"name"[m[K: "Nick Himmel <[01;31m[K"email"[m[K: null>
2017-07-03 18:20:09 -0400

    Update project page so it renders code or photography

--- app/components/Main.jsx
+++ app/components/Main.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import Nav from 'Nav.jsx';
+import Project from 'Project.jsx'
 import Footer from 'Footer.jsx';
 
 
 const Main = (props) => {
-  debugger
     return (
       <div className="main">
         <Nav />
+        <Project project={props.params.project}/>
         <Footer />
       </div>
     );
